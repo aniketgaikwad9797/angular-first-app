b0ef62f3f6c1769203741a13fad1756e701d9c1e
Aniket Gaikwad <aniketgaikwad9797@gmial.com>
2023-07-25 15:57:31 +0530

    added servers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
