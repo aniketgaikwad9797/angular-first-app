ef58e5daaf8c924fbc7849dd9738a3ab5f0d422d
Aniket Gaikwad <aniketgaikwad9797@gmial.com>
2023-07-28 23:30:48 +0530

    string interpolation, property binding, reacting to user event and two way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
+  serverId: number = 26;
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No servers are created';
+  serverName = '';
 
+  createServer() {
+    this.serverCreationStatus =
+      'A server was created, server name is ' + this.serverName;
+    console.log(this.serverCreationStatus);
+  }
+
+  updateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
 }
