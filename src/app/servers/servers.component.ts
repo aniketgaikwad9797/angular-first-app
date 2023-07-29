5f5357e42118526b2197e7edc5bed45fb66e0d25
Aniket Gaikwad <aniketgaikwad9797@gmial.com>
2023-07-29 12:09:32 +0530

    ng if structural directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,11 +10,17 @@ export class ServersComponent {
   allowNewServer: boolean = false;
   serverCreationStatus = 'No servers are created';
   serverName = '';
+  serverCreationFlag = false;
 
   createServer() {
+    this.serverCreationFlag = true;
     this.serverCreationStatus =
       'A server was created, server name is ' + this.serverName;
     console.log(this.serverCreationStatus);
+  }
+
+  resetServer(){
+    
   }
 
   updateServerName(event: any) {
