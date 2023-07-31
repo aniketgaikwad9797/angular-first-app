d576f7c3d633d670325d670e3a7448b438162096
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-08-01 01:46:08 +0530

    directives ngStyle and ngClass

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-directives',
   templateUrl: './directives.component.html',
-  styleUrls: ['./directives.component.css']
+  styleUrls: ['./directives.component.css'],
 })
 export class DirectivesComponent {
+  serverId: number = 25;
+  serverStatus: string = 'online';
 
+  getColor() {
+    return this.serverStatus == 'offline' ? 'red' : 'green';
+  }
 }
